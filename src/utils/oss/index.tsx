2343572f54b27a0d6c82813f7ef665065b4db9f0
jinamin <13587760358@163.com>
2022-03-24 15:13:53 +0800

    feat(test): test

--- src/utils/oss/index.tsx
+++ src/utils/oss/index.tsx
@@ -10,15 +10,9 @@ const VITE_ACCESS_KEY_SECRET =
     ? import.meta.env.VITE_ACCESS_KEY_SECRET
     : process.env.VITE_ACCESS_KEY_SECRET
 
-console.log(process.env.VITE_ACCESS_KEY_ID, 'process.env.VITE_ACCESS_KEY_ID')
-console.log(
-  process.env.VITE_ACCESS_KEY_SECRET,
-  'process.env.VITE_ACCESS_KEY_SECRET'
-)
-
 const client = new OSS({
-  accessKeyId: process.env.VITE_ACCESS_KEY_ID,
-  accessKeySecret: process.env.VITE_ACCESS_KEY_SECRET,
+  accessKeyId: VITE_ACCESS_KEY_ID,
+  accessKeySecret: VITE_ACCESS_KEY_SECRET,
   regoin: 'oss-cn-hangzhou',
   endpoint: 'oss-cn-hangzhou.aliyuncs.com',
   bucket: 'capacity-platform',
