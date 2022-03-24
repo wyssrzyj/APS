6b29707473c994b20bc986a32212ec49f4a532ce
jinamin <13587760358@163.com>
2022-03-24 14:37:55 +0800

    feat(test): test

--- src/utils/oss/index.tsx
+++ src/utils/oss/index.tsx
@@ -1,14 +1,24 @@
 import * as OSS from 'ali-oss'
 
 const VITE_ACCESS_KEY_ID =
-  import.meta && import.meta.env && import.meta.env.VITE_ACCESS_KEY_ID
+  import.meta && import.meta.env
     ? import.meta.env.VITE_ACCESS_KEY_ID
     : process.env.VITE_ACCESS_KEY_ID
 
+console.log(
+  '🚀 ~ file: index.tsx ~ line 4 ~ VITE_ACCESS_KEY_ID',
+  VITE_ACCESS_KEY_ID
+)
+
 const VITE_ACCESS_KEY_SECRET =
-  import.meta && import.meta.env && import.meta.env.VITE_ACCESS_KEY_SECRET
+  import.meta && import.meta.env
     ? import.meta.env.VITE_ACCESS_KEY_SECRET
     : process.env.VITE_ACCESS_KEY_SECRET
+
+console.log(
+  '🚀 ~ file: index.tsx ~ line 15 ~ VITE_ACCESS_KEY_SECRET',
+  VITE_ACCESS_KEY_SECRET
+)
 
 const client = new OSS({
   accessKeyId: VITE_ACCESS_KEY_ID,
