94b067d09d4a603ac39bca75ccbedab61ae1451a
卢英杰 <9433298+lyjlol@user.noreply.gitee.com>
2022-04-20 14:56:53 +0800

    feat: 合并了dev—lyj  添加了 甘特图缓存清除问题

--- src/pages/practice/administration/components/scheduling/toPlan/popup/index.tsx
+++ src/pages/practice/administration/components/scheduling/toPlan/popup/index.tsx
@@ -272,7 +272,7 @@ function Popup(props: { content: any }) {
               >
                 <DatePicker
                   style={{ width: '100%' }}
-                  // disabledDate={disabledDate}
+                  // disabledDate={disabledDate}.
                 />
               </Form.Item>
             </Col>
