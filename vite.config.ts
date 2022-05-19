07fc7b479437c2a487e3d688cc73e55b13b7cb2b
卢英杰 <9433298+lyjlol@user.noreply.gitee.com>
2022-05-19 09:25:25 +0800

    feat: 上测试

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: 卢英杰 9433298+lyjlol@user.noreply.gitee.com
  * @Date: 2022-03-02 15:41:46
  * @LastEditors: 卢英杰 9433298+lyjlol@user.noreply.gitee.com
- * @LastEditTime: 2022-05-19 09:23:23
+ * @LastEditTime: 2022-05-19 09:25:06
  * @FilePath: \jack-aps\vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
