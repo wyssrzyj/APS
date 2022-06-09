6f6ee06cd89dd69506720404d449b4fd9479ace9
卢英杰 <9433298+lyjlol@user.noreply.gitee.com>
2022-06-09 16:17:12 +0800

    feat: 添加了判断

--- src/pages/management/components/scheduling/forms/index.tsx
+++ src/pages/management/components/scheduling/forms/index.tsx
@@ -2,7 +2,7 @@
  * @Author: 卢英杰 9433298+lyjlol@user.noreply.gitee.com
  * @Date: 2022-03-10 15:20:21
  * @LastEditors: lyj
- * @LastEditTime: 2022-06-09 14:56:35
+ * @LastEditTime: 2022-06-09 16:14:20
  * @FilePath: \jack-aps\src\pages\practice\administration\components\scheduling\forms\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,11 +22,14 @@ const HeaderForm = (props: { FormData: any }) => {
   useEffect(() => {
     getData()
   }, [])
-  //本地是否有
   const getCurrentUser = (arr) => {
     const currentUser = JSON.parse(localStorage.getItem('currentUser'))
     if (currentUser) {
-      return currentUser.user.factoryId
+      if (currentUser.user.factoryId !== null) {
+        return currentUser.user.factoryId
+      } else {
+        return arr[0].id
+      }
     } else {
       return arr[0].id
     }
