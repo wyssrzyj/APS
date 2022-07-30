25b7e7e24dc43d81192bc54d16bfcbd4b57efb68
卢英杰 <9433298+lyjlol@user.noreply.gitee.com>
2022-07-30 14:03:18 +0800

    home

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-02 15:41:46
- * @LastEditTime: 2022-07-30 13:28:56
+ * @LastEditTime: 2022-07-30 14:02:41
  * @LastEditors: lyj
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \jack-aps\src\components\Icon\index.tsx
@@ -12,7 +12,7 @@ import { createFromIconfontCN } from '@ant-design/icons'
 // const Icon = createFromIconfontCN({ scriptUrl: url })//icon
 
 const Icon = createFromIconfontCN({
-  scriptUrl: 'iconFont/iconfont.js'
+  scriptUrl: '/iconFont/iconfont.js'
 })
 
 export default Icon
